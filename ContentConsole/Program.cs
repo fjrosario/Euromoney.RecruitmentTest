ab824dd20f3078e46e5b999c27f004f3584ffef7
Frank Rosario <FRosario@gmail.com>
2017-01-05 11:23:55 -0500

    Story 3 complete

--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -18,11 +18,13 @@ namespace ContentConsole
 
             var analyzer = new TextAnalyzer.Analyzer(negativeWordsCleanedUp);
 
-            var results = analyzer.AnalyzeText(content);
+            var results = analyzer.CensorText(content);
 
             Console.WriteLine("Scanned the text:");
             Console.WriteLine(results.Text);
             Console.WriteLine("Total Number of negative words: " + results.NegativeWordCount);
+            Console.WriteLine("Censored Text:");
+            Console.WriteLine(results.CensoredText);
 
             Console.WriteLine("Press ANY key to exit.");
             Console.ReadKey();
--- TextAnalyzer/Analyzer.cs
+++ TextAnalyzer/Analyzer.cs
@@ -14,20 +14,69 @@ namespace TextAnalyzer
             _negativeWords = negativeWords.Select(s => s.ToLower());
         }
 
-        public AnalysisResults AnalyzeText(string text)
+        public IEnumerable<string> GetNegativeWordsInText(string text)
         {
             var wordsInText = ExtractWordsFromText(text);
 
-            var negativeWordsInTextCount = wordsInText.Count(_negativeWords.Contains);
+            var negativeWordsInText = wordsInText.Where(_negativeWords.Contains);
+
+            return negativeWordsInText.ToList();
+        }
+
+        public AnalysisResults AnalyzeText(string text)
+        {
+            var negativeWordsInText = GetNegativeWordsInText(text);
 
             var result = new AnalysisResults
             {
                 Text = text,
-                NegativeWordCount = negativeWordsInTextCount
+                NegativeWordCount = negativeWordsInText.Count()
             };
 
             return result;
         }
+
+        public string CensorWord(string word)
+        {
+            const char CENSORED_CHAR = '#';
+            if (string.IsNullOrWhiteSpace(word) || word.Length < 2) return word;
+            char first = word[0];
+            char last = word[word.Length - 1];
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(first);
+            for(int x=0; x<word.Length-2;x++)
+            {
+                sb.Append(CENSORED_CHAR);
+            }
+            sb.Append(last);
+
+            return sb.ToString();
+        }
+
+        public ReplacementResults CensorText(string text)
+        {
+            var negativeWordsInText = GetNegativeWordsInText(text);
+
+            var replacementText = text;
+
+            var result = new ReplacementResults
+            {
+                Text = text,
+                NegativeWordCount = negativeWordsInText.Count()
+            };
+
+            foreach(var negativeWord in negativeWordsInText)
+            {
+                replacementText = replacementText.Replace(negativeWord, CensorWord(negativeWord));
+            }
+
+            result.CensoredText = replacementText;
+
+            return result;
+        }
+
+
         public IEnumerable<string> ExtractWordsFromText(string text)
         {
             var wordsWithPunctuation = System.Text.RegularExpressions.Regex.Split(text, @"\W+").ToList().Select(s => s.ToLower());
