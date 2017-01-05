9d3d6b1c77beb6abc51b9dc1c436c0d1b46f9fb6
Frank Rosario <FRosario@gmail.com>
2017-01-05 10:58:23 -0500

    User story 1 complete

---
+++ TextAnalyzer/Analyzer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzer
+{
+    public class Analyzer
+    {
+        IEnumerable<string> _negativeWords;
+        public Analyzer()
+        {
+            _negativeWords = new string[] { "swine","bad","nasty","horrible"}.Select(s => s.ToLower());
+        }
+
+        public AnalysisResults AnalyzeText(string text)
+        {
+            var wordsInText = ExtractWordsFromText(text);
+
+            var negativeWordsInTextCount = wordsInText.Count(_negativeWords.Contains);
+
+            var result = new AnalysisResults
+            {
+                Text = text,
+                NegativeWordCount = negativeWordsInTextCount
+            };
+
+            return result;
+        }
+        public IEnumerable<string> ExtractWordsFromText(string text)
+        {
+            var wordsWithPunctuation = System.Text.RegularExpressions.Regex.Split(text, @"\W+").ToList().Select(s => s.ToLower());
+            return wordsWithPunctuation;
+        }
+
+}
+}
